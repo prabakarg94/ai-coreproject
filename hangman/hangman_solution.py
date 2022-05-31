f2715f9c27c412d52176e4baca7deee3549d59c6
prabakarg94 <101649383+prabakarg94@users.noreply.github.com>
2022-05-31 21:10:38 +0530

    Update hangman_solution.py

--- hangman/hangman_solution.py
+++ hangman/hangman_solution.py
@@ -46,6 +46,9 @@ class Hangman:
         # 1. "The mystery word has {len(self.word)} characters" (The number of letters is NOT the UNIQUE number of letters)
         # 2. {word_guessed}
         
+        print("The mystery word has", {len(word)}, "characters")
+        print(word_guessed)
+        
         pass
 
         
